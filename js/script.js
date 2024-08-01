f9cd6177d79cdcf8f717c09e93950b0d491b2b21
Syed Towhid Hussain <stowhidm@gmail.com>
2024-08-01 22:36:06 +0530

    done

--- js/script.js
+++ js/script.js
@@ -26,4 +26,23 @@ window.onscroll=()=>
 }
 
 
+ var swiper = new Swiper(".product-slider", {
+      loop:true,
+      spaceBetween: 20,
 
+      autoplay: {
+      	delay: 4000,
+      	disableOnInteraction: false,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1020: {
+          slidesPerView: 3,
+        },
+      },
+    });
